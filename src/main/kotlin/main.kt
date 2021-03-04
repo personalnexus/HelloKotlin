a5d7dd2deb38cd24994d7a1d6d0cff385814f638
StefanH <personalnexus@users.noreply.github.com>
2021-03-04 17:56:51 +0100

    array instead of map for faster lookup

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
 class Node<V> {
-    val childrenByCharacter: MutableMap<Char, Node<V>> = HashMap()
+    val childrenByCharacter: Array<Node<V>?> = Array(36) { null }
     var value: V? = null
     var hasValue: Boolean = false
 }
@@ -14,10 +14,13 @@ class Trie<V> {
     fun add(key: String, value: V?): Pair<Boolean, V?> {
         var node = root
         for (char in key) {
-            node = node.childrenByCharacter.getOrElse(char, {
-                val newNode = Node<V>()
-                node.childrenByCharacter[char] = newNode
-                newNode})
+            val index = getIndexOfCharacter(char)
+            var childNode = node.childrenByCharacter[index]
+            if (childNode == null) {
+                childNode = Node<V>()
+                node.childrenByCharacter[index] = childNode
+            }
+            node = childNode
         }
         val result = node.toPair()
         node.value = value
@@ -25,24 +28,37 @@ class Trie<V> {
         return result
     }
 
+    private fun getIndexOfCharacter(char: Char): Int {
+        if ((char >= '1') && (char <= '0')) {
+            return char.toByte() - 48
+        } else if ((char >= 'A') && (char <= 'Z')){
+            return char.toByte() - 55
+        }
+        throw IllegalArgumentException("Invalid character in key $char")
+
+    }
+
     fun get(key: String): Pair<Boolean, V?>{
         var node: Node<V>? = root
         for (char in key){
-            node = node!!.childrenByCharacter[char] ?: break
+            val index = getIndexOfCharacter(char)
+            node = node!!.childrenByCharacter[index] ?: break
         }
         return node.toPair()
     }
 }
 
 fun main() {
-    val trie = Trie<Int>()
-    println("Added one to 1: ${trie.add("one", 1)}")
-    println("Added two to 22: ${trie.add("two", 22)}")
-    println("Added null to null: ${trie.add("null", null)}")
-    println("Added two to 2: ${trie.add("two", 2)}")
 
-    println("Getting one ${trie.get("one")}")
-    println("Getting two ${trie.get("two")}")
-    println("Getting null ${trie.get("null")}")
-    println("Getting three ${trie.get("three")}")
+
+    val trie = Trie<Int>()
+    println("Added one to 1: ${trie.add("ONE", 1)}")
+    println("Added two to 22: ${trie.add("TWO", 22)}")
+    println("Added null to null: ${trie.add("NULL", null)}")
+    println("Added two to 2: ${trie.add("TWO", 2)}")
+
+    println("Getting one ${trie.get("ONE")}")
+    println("Getting two ${trie.get("TWO")}")
+    println("Getting null ${trie.get("NULL")}")
+    println("Getting three ${trie.get("THREE")}")
 }
